595eb52a7e16d677a72094942d8bdfe1d5e8a6f7
LuisFe <lopezf@cicese.edu.mx>
2020-08-22 07:29:23 -0700

    Python_web update
    Form

--- python_web/src/index.py
+++ python_web/src/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import os
 
 app = Flask (__name__)
@@ -15,6 +15,16 @@ def about():
 @app.route('/us')
 def us():
     return render_template('us.html')
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        message = "Welcome: " + username
+    return render_template('form.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
