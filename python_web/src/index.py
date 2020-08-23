ccf37bf36f7693e29dd60c1d8b934b55fed6c36d
LuisFe <lopezf@cicese.edu.mx>
2020-08-22 23:42:04 -0700

    Python_web update

--- python_web/src/index.py
+++ python_web/src/index.py
@@ -7,7 +7,6 @@ app = Flask (__name__)
 def home():
     return render_template('home.html')
 
-
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -16,15 +15,20 @@ def about():
 def us():
     return render_template('us.html')
 
-@app.route("/form", methods=["GET", "POST"])
-def form():
-    message = ''
+@app.route("/signup", methods=["GET", "POST"])
+def sign_up():
+    username=False
     if request.method == 'POST':
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
         message = "Welcome: " + username
-    return render_template('form.html', message=message)
+    if username:
+        return render_template('sign_up.html', message=message)
+    else:
+        return render_template('sign_up.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
